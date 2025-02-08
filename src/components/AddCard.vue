02374c6d5c70858fe12baddeb0344a0fda97d19a
soyUnaGata <fallen.snitch@gmail.com>
2025-02-08 20:50:56 +0200

    implement add-card

--- src/components/AddCard.vue
+++ src/components/AddCard.vue
@@ -14,9 +14,11 @@ const show = ref(true)
 
 const saveCard = async (event) => {
   event.preventDefault()
-  alert(JSON.stringify(prefCard.value))
-await PrefCardsService.addPrefCard(prefCard.value);
-
+try {
+  await PrefCardsService.addPrefCard(prefCard.value);
+}catch(error) {
+    console.log(error);
+}
 }
 
 const onReset = async (event) => {
@@ -32,6 +34,13 @@ const onReset = async (event) => {
 </script>
 
 <template>
+
+  <RouterLink :to="{name: 'cards'}">
+    <svg width="24" height="24" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" fill="green">
+      <path d="M177.5 414c-8.8 3.8-19 2-26-4.6l-144-136C2.7 268.9 0 262.6 0 256s2.7-12.9 7.5-17.4l144-136c7-6.6 17.2-8.4 26-4.6s14.5 12.5 14.5 22v72h288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H192v72c0 9.6-5.7 18.2-14.5 22z"/>
+    </svg>
+  </RouterLink>
+
   <div>
     <b-form @submit="saveCard" @reset="onReset" v-if="show">
       <b-form-group
@@ -60,7 +69,7 @@ const onReset = async (event) => {
         <b-form-input
             id="input-2"
             v-model="prefCard.duration"
-            placeholder="Enter operation"
+            placeholder="Enter duration"
             required
         ></b-form-input>
       </b-form-group>
@@ -69,7 +78,7 @@ const onReset = async (event) => {
         <b-form-input
             id="input-2"
             v-model="prefCard.tools"
-            placeholder="Enter operation"
+            placeholder="Enter tools"
             required
         ></b-form-input>
       </b-form-group>
