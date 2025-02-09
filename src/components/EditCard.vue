aa79420962ee745a645679d26e90bbf4a8102ef9
soyUnaGata <fallen.snitch@gmail.com>
2025-02-09 12:01:40 +0200

    implement return-button

--- src/components/EditCard.vue
+++ src/components/EditCard.vue
@@ -3,6 +3,8 @@ import {BForm} from "bootstrap-vue-3";
 import {useRoute} from "vue-router";
 import PrefCardsService from "../../service/prefcards-service";
 import {onMounted, ref} from "vue";
+import router from "@/router";
+import ReturnButton from "@/components/shared/ReturnButton.vue";
 
 const route = useRoute();
 const prefCardId = ref(route.params.id);
@@ -24,6 +26,10 @@ const updatePrefCard = async (event) => {
   }
 }
 
+const cancelForm = async () => {
+  await router.push({name: 'cards'});
+}
+
 
 onMounted(async () => {
   await getPrefCard();
@@ -31,11 +37,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <RouterLink :to="{name: 'cards'}">
-  <svg width="24" height="24" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" fill="green">
-    <path d="M177.5 414c-8.8 3.8-19 2-26-4.6l-144-136C2.7 268.9 0 262.6 0 256s2.7-12.9 7.5-17.4l144-136c7-6.6 17.2-8.4 26-4.6s14.5 12.5 14.5 22v72h288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H192v72c0 9.6-5.7 18.2-14.5 22z"/>
-  </svg>
-</RouterLink>
+  <return-button @click="cancelForm" />
 
   <div>
     <b-form @submit="updatePrefCard">
@@ -79,8 +81,8 @@ onMounted(async () => {
         ></b-form-input>
       </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <b-button type="submit" variant="success">Submit</b-button>
+      <b-button type="button" variant="danger" @click="cancelForm">Cancel</b-button>
     </b-form>
   </div>
 </template>
