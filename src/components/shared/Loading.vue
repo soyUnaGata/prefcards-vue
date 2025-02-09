44ad53dbf265bffcd8efb1fc6a038a4c8bc6f788
soyUnaGata <fallen.snitch@gmail.com>
2025-02-09 19:11:16 +0200

    implement loader

---
+++ src/components/shared/Loading.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="loader" v-if="loading">
+    <h3>We use free recourses, so it might take time to load all the information. Thank you for waiting!</h3>
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.loader {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+}
+
+.spinner {
+  width: 50px;
+  height: 50px;
+  border: 5px solid #fff;
+  border-top-color: transparent;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
